62a4787ccb17aa3a398e88fe400131596f23f329
Pavel Manolev <PavelManolev@abv.bg>
2015-09-02 14:11:19 +0300

    Модул2.Допълнителни оператори

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -14,8 +14,8 @@ namespace Modul02
 
 			//Събиране на числа
 
-			a = 55;
-			b = 10;
+			a = 10;
+			b = 5;
 
 			sum = a + b;
 
@@ -26,6 +26,32 @@ namespace Modul02
 			Console.Write ( " = ");
 			Console.WriteLine (sum ); 
 
+			//Допълнителни оператори
+			Console.WriteLine ("\nИзползване на +=");
+			a += b; 
+			Console.WriteLine ( a );
+
+			Console.WriteLine ("\nИзползване на -=");
+			a -= b; 
+			Console.WriteLine ( a );
+
+			Console.WriteLine ("\nИзползване на *=");
+			a *= b; 
+			Console.WriteLine ( a );
+
+			Console.WriteLine ("\nИзползване на ++");
+			a ++; 
+			Console.WriteLine ( a );
+
+			a = 15; b=10;
+
+			Console.WriteLine ("\nStandartno Delene");
+
+			Console.WriteLine ( a/b ); //1
+
+			Console.WriteLine ("\nHwashtane na ostatyk");
+
+			Console.WriteLine ( a%b ); //5
 }
 	}
 }
