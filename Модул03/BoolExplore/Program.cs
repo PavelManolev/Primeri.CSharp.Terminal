7a9c8502b7bf00c9bd85f083d9600309eb706df6
Pavel Manolev <PavelManolev@abv.bg>
2015-09-11 19:39:31 +0300

    Модул3.Видео7 Примери с променливи bool-2If operator

--- Модул03/BoolExplore/Program.cs
+++ Модул03/BoolExplore/Program.cs
@@ -10,35 +10,35 @@ namespace BoolExplore
 
 			bool _test =5>10;
 			int[] _i=new int[] {4,3,2};
-			int _ui = Convert.ToInt32 (Console.ReadLine ());
+			int _ui = 0; //Convert.ToInt32 (Console.ReadLine ());
 
-//
-//			bool _check01 = _ui <= _i.Length;
-//			bool _check02 = _ui>0;
-//
-//			bool _exp01 = _check01 && _check02;	//логическо И "&&"
-//			bool _exp02 = _check01 || _check02;		//логическо ИЛИ "||"
-//			bool _exp03 = !_check01;		// отрицание "!"
-//
-//
-//
-//
-//			Console.WriteLine ("Logical I"+ _exp01.ToString ());
-//			Console.WriteLine ("Лигическо ИЛИ"+ _exp02.ToString ());
-//			Console.WriteLine ("Отрицание на "+_check01.ToString ()+ ": "+_exp03);
+			string _input="0";
 
-			if (_ui > 0 && _ui <= _i.Length) {
+			Console.Write ("Моля въведете индекс: ");
+			_input = Console.ReadLine ();
+
+
+
+			bool _check01 = int.TryParse (_input, out _ui);
+
+			bool _check02 = _ui <= _i.Length;
+			bool _check03 = _ui>0;
+
+			if (_check01 && _check02 && _check03) {
 				Console.WriteLine (_i [_ui - 1]);
-			} else {
-				if (_ui> _i.Length)
-				{
-					Console.WriteLine ("Индекса е прекалено голям\n\n\n");
-				}
-				if (_ui <= 0)
-				{
-					Console.WriteLine ("Индекса е прекалено малък \n\n\n");
-				}
 			}
+			if (!_check01) {
+				Console.WriteLine ("Индекса трябва да бъде цяло число\n\n\n");}
+
+			if (!_check02) {
+				Console.WriteLine ("Инекса е прекалено голям\n\n\n");}
+			if (!_check03 && _check01) {
+				Console.WriteLine ("Индекса е прекалено Малък\n\n\n");}
+
+//
+//		
+
+		
 
 
 
