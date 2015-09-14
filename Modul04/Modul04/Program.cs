b7af723adbc32bcba2f60089013cecc3182422ff
Pavel Manolev <PavelManolev@abv.bg>
2015-09-14 19:34:29 +0300

    Модул4.Видео3 Влагане на цикли

--- Modul04/Modul04/Program.cs
+++ Modul04/Modul04/Program.cs
@@ -6,17 +6,26 @@ namespace Modul04
 	{
 		public static void Main (string[] args)
 		{
-			String[] _table = {"Иван", "Гошо", "Гергана" };
+			String[ , ] _table = {
+				{"Иван",   "Георгиев ", "19" },
+				{"Васил",   "Праматаров ", "23" },
+				{"Павел",   "Манолев ", "37" },
+				{"Момчил",   "Пеков ", "33" },
+				{"Петя",   "Коритарова ", "28" },
+				{"Ева",   "Александрова ", "35" }};
 			//Антетка на таблица
-			Console.WriteLine ("Име");
-			Console.WriteLine ("=========");
+			Console.WriteLine ("Име".PadRight(14)+"Фамилия".PadRight(14)+"Години".PadRight(14));
+			Console.WriteLine ("========================================");
 
+			Console.WriteLine (_table.Length);
 			// Тяло на таблица
-			for ( int i=0; i<_table.Length ; i++ )
+			for ( int i=0; i<_table.Length/3 ; i++ )
 			{
-				//Код за цикъл
-				Console.WriteLine (_table [i]);
-
+				for (int j = 0; j < 3; j++)
+				{
+					Console.Write ( _table[i,j].PadRight(14));
+				}
+				Console.WriteLine ();
 			}
 		}
 	}
