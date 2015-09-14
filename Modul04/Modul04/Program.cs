c138587869f04dd192d5f4f06fd9d049d425fc23
Pavel Manolev <PavelManolev@abv.bg>
2015-09-14 18:57:05 +0300

    Модул4. Видео2 - For примери

---
+++ Modul04/Modul04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Modul04
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			String[] _table = {"Иван", "Гошо", "Гергана" };
+			//Антетка на таблица
+			Console.WriteLine ("Име");
+			Console.WriteLine ("=========");
+
+			// Тяло на таблица
+			for ( int i=0; i<_table.Length ; i++ )
+			{
+				//Код за цикъл
+				Console.WriteLine (_table [i]);
+
+			}
+		}
+	}
+}
