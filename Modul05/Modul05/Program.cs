2980a695f4cb60f87e243849a51693b1fdbff895
Pavel Manolev <PavelManolev@abv.bg>
2015-10-09 16:51:25 +0300

    Модул5.Видео2, Референции в Модулите

--- Modul05/Modul05/Program.cs
+++ Modul05/Modul05/Program.cs
@@ -18,6 +18,32 @@ namespace Modul05
 				if (usercommand.Contains ("cmd01")) cmd01 (usercommand); 
 				if (usercommand.Contains ("cmd02")) Console.WriteLine (cmd02 ( usercommand ) ); 
 
+				if (usercommand.Contains ("cmd03"))
+				{
+					double _a=0;
+
+						if (cmd03(usercommand, out _a))
+						{
+							Console.WriteLine ("a . a = "+ _a.ToString ());
+					}else {
+						Console.WriteLine ("Командата не е въведена коректно");
+					}
+				}
+
+				if (usercommand.Contains ("cmd04")) 
+				{
+					double _t=5, _c=-2;
+					if (cmd04 ( ref _t)) 
+					{
+						Console.WriteLine (_t);
+					}
+
+					if (cmd04 (ref _c)) 
+					{
+						Console.WriteLine (0);
+					}
+
+				}
 			} while (usercommand != "exit");
 
 
@@ -27,7 +53,7 @@ namespace Modul05
 
 		//Методи. Само да се изпълнят
 
-		public static void cmd01 ( string _input )  //cmd01 5.5^2
+		public static void cmd01 ( string _input )  //cmd01 5=5^2 --->25
 		{
 			try 
 			{ 
@@ -36,7 +62,7 @@ namespace Modul05
 				if (double.TryParse (_input.Split(' ')[1], out _temp ) )
 				{
 					Console.WriteLine ("\nРезултат от"+ _temp.ToString()+ "^2: "+(_temp * _temp).ToString () );
-				}
+				} 
 
 			}catch {
 			}
@@ -46,7 +72,7 @@ namespace Modul05
 
 		//методи. Да върнат параметри
 
-		public static int cmd02 (string _input) //cmd02 5   --->5*5*5=125
+		public static int cmd02 (string _input) //cmd02 5 = 5^3   --->5*5*5=125
 		{
 			int _temp=0;
 
@@ -57,6 +83,39 @@ namespace Modul05
 
 			return _temp;
 		}
+
+		public static bool cmd03 (string _input, out double _i) //cmd03 40 --> 45^2
+		{
+			try {
+				string _p= _input.Split (' ')[1];  //cmd03, cmd03   43,
+				double __p = 0;
+
+				if (double.TryParse ( _p, out __p))
+				{
+					_i= __p* __p;
+						return true;
+				}
+
+
+			}catch{
+			}
+
+			_i = 0;
+
+			return false;
+
+		}
+		public static bool cmd04 (ref double _i)
+		{
+
+			Double _temp = _i;
+			_i = _i * _i;
+
+
+
+			return (_temp > 0);
+		}
+
 
 	}
 }
