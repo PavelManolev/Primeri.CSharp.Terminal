67c346f703378004b7f7b427517fae347338a527
Pavel Manolev <PavelManolev@abv.bg>
2016-01-02 16:54:47 +0200

    Modul5ур..Урок 4.Въведение Класове

---
+++ Modul05/ClassExplorer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassExplorer
+{
+	class person
+	{
+		public string simpleString  = "Тестова променлива от клас";
+		public person ()
+		{
+			simpleString = "Промяна на промелива при инициализиране";
+		}
+
+
+
+	}
+
+
+	class MainClass
+	{
+	 
+		public static void Main (string[] args)
+		{
+			//Дефиниране на клас
+			person _person = new person ();
+			//Достъпване на клас
+			Console.WriteLine ( _person.simpleString );
+		}
+	}
+}
