12e41589487dd80872c5c097ef43144371f5ea0c
Pavel Manolev <PavelManolev@abv.bg>
2016-01-09 17:04:48 +0200

    Modul5.Video 9.Рекурсиви

---
+++ Modul05/Fact/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fact
+{
+	class MainClass
+	{
+		//5!=5*4*3*2*1; 0!=1
+		public static int Fact (int _fact)
+		{
+			if (_fact <= 1)
+				return 1;
+			else
+				return _fact * Fact (_fact - 1);
+
+			//2!   2 * 1
+			//3!   3 * 2 * 1
+		}
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine (Fact (3));
+			Console.WriteLine (Fact (4));
+			Console.WriteLine (Fact (0));
+			Console.WriteLine (Fact (2));
+			Console.WriteLine (Fact (8));
+		}
+	}
+}
